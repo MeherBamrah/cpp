2d29faa97befe1ff1bd3f4891e96d3556a5dfdbe
Meher_Codez <156418981+MeherBamrah@users.noreply.github.com>
2025-09-03 17:44:30 +0530

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -4,74 +4,48 @@
 #include <string>
 using namespace std;
 
-// Base class
-class Palindrome {
-public:
-    virtual bool isPalindrome() = 0; // Pure virtual function
-};
-
-// Derived class to check number palindrome
-class NumberPalindrome : public Palindrome {
-private:
-    int number;
-public:
-    NumberPalindrome(int n) {
-        number = n;
+// Function to check if a number is a palindrome
+bool isNumberPalindrome(int num) {
+    int original = num, reversed = 0, digit;
+    while (num > 0) {
+        digit = num % 10;
+        reversed = reversed * 10 + digit;
+        num /= 10;
     }
+    return (original == reversed);
+}
 
-    bool isPalindrome() override {
-        int original = number, reversed = 0, digit;
-        while (number > 0) {
-            digit = number % 10;
-            reversed = reversed * 10 + digit;
-            number /= 10;
-        }
-        return original == reversed;
+// Function to check if a string is a palindrome
+bool isStringPalindrome(const string &str) {
+    int left = 0, right = str.length() - 1;
+    while (left < right) {
+        if (str[left] != str[right])
+            return false;
+        left++;
+        right--;
     }
-};
-
-// Derived class to check string palindrome
-class StringPalindrome : public Palindrome {
-private:
-    string str;
-public:
-    StringPalindrome(string s) {
-        str = s;
-    }
-
-    bool isPalindrome() override {
-        int start = 0, end = str.length() - 1;
-        while (start < end) {
-            if (str[start] != str[end])
-                return false;
-            start++;
-            end--;
-        }
-        return true;
-    }
-};
+    return true;
+}
 
 int main() {
-    int num;
-    string text;
+    int number;
+    string word;
 
+    // Check number palindrome
     cout << "Enter a number: ";
-    cin >> num;
-    Palindrome* numCheck = new NumberPalindrome(num);
-    if (numCheck->isPalindrome())
-        cout << num << " is a palindrome number." << endl;
+    cin >> number;
+    if (isNumberPalindrome(number))
+        cout << number << " is a palindrome number." << endl;
     else
-        cout << num << " is not a palindrome number." << endl;
-    delete numCheck;
+        cout << number << " is NOT a palindrome number." << endl;
 
-    cout << "\nEnter a string: ";
-    cin >> text;
-    Palindrome* strCheck = new StringPalindrome(text);
-    if (strCheck->isPalindrome())
-        cout << text << " is a palindrome string." << endl;
+    // Check string palindrome
+    cout << "Enter a string: ";
+    cin >> word;
+    if (isStringPalindrome(word))
+        cout << "\"" << word << "\" is a palindrome string." << endl;
     else
-        cout << text << " is not a palindrome string." << endl;
-    delete strCheck;
+        cout << "\"" << word << "\" is NOT a palindrome string." << endl;
 
     return 0;
 }
