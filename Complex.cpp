cc48269badc15944b75ec9d2fec5d6b578154831
Meher_Codez <156418981+MeherBamrah@users.noreply.github.com>
2025-09-22 20:04:26 +0530

    Update Complex.cpp

--- Complex.cpp
+++ Complex.cpp
@@ -1,9 +1,10 @@
 //Create a class called Complex for performing following operations:
 //(a) Overload increment and decrement operators for increasing and decreasing complex number values (Unary operator overload).
 //(b) Overload ‘+’ op and ‘-‘op for complex numbers (Binary operator overloading).
+
+
 #include <iostream>
 using namespace std;
-
 class Complex {
 private:
     float real;
@@ -16,31 +17,28 @@ public:
         imag = i;
     }
 
-    // Display function
+    // function
     void display() {
         cout << real << " + " << imag << "i" << endl;
     }
 
-    // Unary operator overloading for increment
-    Complex operator++() {      // Pre-increment
+    Complex operator++() {      
         ++real;
         ++imag;
-        return *this;           // Returning updated object
+        return *this;        
     }
 
-    Complex operator--() {      // Pre-decrement
+    Complex operator--() {     
         --real;
         --imag;
         return *this;
     }
 
-    // Binary operator overloading for addition
-    Complex operator+(const Complex &c) {
+     Complex operator+(const Complex &c) {
         return Complex(real + c.real, imag + c.imag);
     }
 
-    // Binary operator overloading for subtraction
-    Complex operator-(const Complex &c) {
+     Complex operator-(const Complex &c) {
         return Complex(real - c.real, imag - c.imag);
     }
 };
@@ -52,19 +50,19 @@ int main() {
     c1.display();
 
     cout << "After increment (++c1): ";
-    ++c1;       // Unary increment
+    ++c1;       
     c1.display();
 
     cout << "After decrement (--c1): ";
-    --c1;       // Unary decrement
+    --c1;       
     c1.display();
 
     cout << "c1 + c2 = ";
-    c3 = c1 + c2;    // Binary +
+    c3 = c1 + c2;   
     c3.display();
 
     cout << "c1 - c2 = ";
-    c3 = c1 - c2;    // Binary -
+    c3 = c1 - c2;  
     c3.display();
 
     return 0;
